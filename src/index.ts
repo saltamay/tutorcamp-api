c2fdb7a1c78b67c680e44ddc74b29651f9917a9a
Sal Tamay <sal.tamay@outlook.com>
2021-05-30 19:02:50 -0400

    Add morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import express from 'express';
+// Load env variables
 import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import morgan from 'morgan';
 
 // Import routers
 import bootcamps from './routes/bootcamps';
 
-// Load env variables
-dotenv.config();
+const PORT = process.env.PORT || 8000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
 
+// Dev logging middleware
+if (NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 8000;
-const ENV = process.env.NODE_ENV || 'development';
-
 app.listen(PORT, () =>
-  console.log(`Server running in '${ENV}' mode on port: ${PORT}`)
+  console.log(`Server running in '${NODE_ENV}' mode on port: ${PORT}`)
 );
