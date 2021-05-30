5fff5083c19fc61ccacdfb63e73960038bcc73eb
Sal Tamay <sal.tamay@outlook.com>
2021-05-30 18:04:55 -0400

    Create crud routes for bootcamps

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,42 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.get('/api/v1/bootcamps', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootcamps',
+  });
+});
 
+app.get('/api/v1/bootcamps/:id', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp ${req.params.id}`,
+  });
+});
+
+app.post('/api/v1/bootcamps', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Create a bootcamp',
+  });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ${req.params.id}`,
+  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ${req.params.id}`,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
 const ENV = process.env.NODE_ENV || 'development';
 
 app.listen(PORT, () =>
