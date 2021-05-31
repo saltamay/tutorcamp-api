131a061065d44601c840948375aec6483c08824a
Sal Tamay <sal.tamay@outlook.com>
2021-05-30 20:03:35 -0400

    Connect to mongodb atlas db

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ dotenv.config();
 
 import express from 'express';
 import morgan from 'morgan';
+import db from './config/db';
 
 // Import routers
 import bootcamps from './routes/bootcamps';
 
 const PORT = process.env.PORT || 8000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Connect to database
+db();
 
 const app = express();
 
@@ -20,6 +24,13 @@ if (NODE_ENV == 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in '${NODE_ENV}' mode on port: ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  // Cloase the server & exit
+  server.close(() => process.exit(1));
+});
