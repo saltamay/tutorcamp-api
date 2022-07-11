7898bb0b263bb4e388a18d091bc499ef337a577c
Sal Tamay <sal.tamay@outlook.com>
2022-07-11 16:21:24 -0400

    Add error handling for validation errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,16 +1,46 @@
 import { ErrorResponse } from '../utils/errorResponse.js';
 
 export const errorHandler = (err, req, res, next) => {
+  let errors = [];
+
+  // Default error
   let error = { ...err };
   error.message = err.message;
 
-  console.error(err.stack.red);
+  console.error(err);
 
+  // Mongo invalid ObjectId error
   if (err.name === 'CastError') {
     error = new ErrorResponse(`Invalid resource id: ${err.value}`, 400);
   }
 
+  // Mongo duplicate key error
+  if (err.name === 'MongoServerError' && err.code === 11000) {
+    error = new ErrorResponse(`Validation Error`, 400);
+
+    errors = [
+      {
+        message: 'Bootcamp already exist',
+        path: 'name',
+        value: err.keyValue.name,
+      },
+    ];
+  }
+
+  if (err.name === 'ValidationError') {
+    error = new ErrorResponse(`Validation Error`, 400);
+
+    errors = Object.values(err.errors).map((error) => {
+      const { message, path, value } = error.properties;
+      return {
+        message,
+        path,
+        value,
+      };
+    });
+  }
+
   res
     .status(error.statusCode || 500)
-    .json({ success: false, error: error.message || 'Server Error' });
+    .json({ success: false, message: error.message || 'Server Error', errors });
 };
