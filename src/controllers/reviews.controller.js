b98b3e182b53f9e1428d8bd0d97af560e5f5a46d
Sal Tamay <sal.tamay@outlook.com>
2022-07-19 23:32:51 -0400

    Add GET/ PUT/ and DELETE/ endpoints for Review model

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -41,3 +41,62 @@ export const getReviews = (req, res, next) => {
     return getAllReviews(req, res, next);
   }
 };
+
+/**
+ * @desc    Get a review by id
+ * @route   GET /api/v1/reviews/:id
+ * @access  Public
+ */
+export const getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate(
+    'bootcamp',
+    'name description'
+  );
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review with the id of ${req.params.id} not found`, 404)
+    );
+  }
+
+  res.status(200).json({ success: true, data: review });
+});
+
+/**
+ * @desc    Update a review by id
+ * @route   PUT /api/v1/review/:id
+ * @access  Private
+ */
+export const updateReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review with the id of ${req.params.id} not found`, 404)
+    );
+  }
+
+  res.status(200).json({ success: true, data: review });
+});
+
+/**
+ * @desc    Delete a review by id
+ * @route   DELETE /api/v1/delete/:id
+ * @access  Private
+ */
+export const deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Course with the id of ${req.params.id} not found`, 404)
+    );
+  }
+
+  review.remove();
+
+  res.status(200).json({ success: true, data: review });
+});
--- src/routes/reviews.route.js
+++ src/routes/reviews.route.js
@@ -1,9 +1,16 @@
 import express from 'express';
 const reviewsRouter = express.Router({ mergeParams: true });
-import { getReviews } from '../controllers/reviews.controller.js';
+import { deleteReview, getReview, getReviews, updateReview } from '../controllers/reviews.controller.js';
 import { Review } from '../models/review.model.js';
 import { filterHandler } from '../middleware/filterHandler.js';
+import { authProtect } from '../middleware/authProtect.js';
+import { roleHandler } from '../middleware/roleHandler.js';
 
 reviewsRouter.route('/').get(filterHandler(Review, 'bootcamp'), getReviews);
+reviewsRouter
+  .route('/:id')
+  .get(getReview)
+  .put(authProtect, roleHandler('publisher', 'admin', 'user'), updateReview)
+  .delete(authProtect, roleHandler('publisher', 'admin', 'user'), deleteReview);
 
 export { reviewsRouter };
